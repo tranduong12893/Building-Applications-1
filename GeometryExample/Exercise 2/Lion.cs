78ed7f7f27ec56b223171c4c31fd4478e25ce347
tranduong12893 <73330600+tranduong12893@users.noreply.github.com>
2021-07-21 14:42:44 +0700

    Done

---
+++ GeometryExample/Exercise 2/Lion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Lion : Animal
+    {
+        public Lion()
+        {
+            Name = "Lion";
+            Weight = 200;
+        }
+    }
+}
